b8100136a2c7c159ca590ac69f085044ab6c2194
João Silvério <52933126+joao-silverio-dev@users.noreply.github.com>
2025-06-16 12:31:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+const inputTarefas = document.querySelector('.input-tarefas');
+const botaoTarefas = document.querySelector('.botao-tarefas');
+const tarefas = document.querySelector('.tarefas');
+
+botaoTarefas.addEventListener('click', function(){
+    adicionarTarefa();
+});
+function adicionarTarefa(){
+    if(!inputTarefas.value) return;
+        const tarefa = document.createElement('li');
+        tarefa.innerText = `${inputTarefas.value} `;
+        tarefas.appendChild(tarefa);
+        inputTarefas.value = '';
+        inputTarefas.focus();
+        const botaoApagar = document.createElement('button');
+        botaoApagar.innerText = `Apagar`;
+        botaoApagar.className = `apagador`;
+        tarefa.appendChild(botaoApagar);
+        salvarTarefas();
+}
+document.addEventListener('click', function(e){
+    if(e.target.className === `apagador`){
+        e.target.parentElement.remove();
+        salvarTarefas();
+    }
+});
+
+function salvarTarefas(){
+    const liTarefas = tarefas.querySelectorAll('li');
+    let listaDeTarefas = [];
+
+    for(let tarefa of liTarefas){
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        listaDeTarefas.push(tarefaTexto);
+    }
+
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
+}
+
+function getTarefasSalvas(){
+    const tarefasSalvasJson = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefasSalvasJson);
+
+    for(let tarefaSalva of listaDeTarefas){
+        console.log(tarefaSalva)
+        const tarefa = document.createElement('li');
+        tarefa.innerText = `${tarefaSalva}`;
+        tarefas.appendChild(tarefa);
+        inputTarefas.value = '';
+        inputTarefas.focus();
+        const botaoApagar = document.createElement('button');
+        botaoApagar.innerText = `Apagar`;
+        botaoApagar.className = `apagador`;
+        tarefa.appendChild(botaoApagar);
+        
+    }
+    salvarTarefas();
+}
+getTarefasSalvas();
